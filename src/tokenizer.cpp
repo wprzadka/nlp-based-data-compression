1f7fd0bd9f7f60dfba678ab36f045361680fc6c9
wprzadka <wiktor.przadka.1@gmail.com>
2022-06-26 17:37:45 +0200

    implement word division

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -10,7 +10,7 @@
 
 Tokenizer::Tokenizer(const std::string& vocabulary_path, const std::string& pair_merges_path){
     vocabulary = read_vocabulary(vocabulary_path);
-    pair_merges = read_pair_merges(pair_merges_path);
+    pair_merges_ranks = read_pair_merges(pair_merges_path);
 }
 
 std::map<std::string, int> Tokenizer::read_vocabulary(const std::string & path) {
@@ -31,17 +31,17 @@ std::map<std::string, int> Tokenizer::read_vocabulary(const std::string & path) 
     return vocab;
 }
 
-std::map<std::pair<std::string, std::string>, int> Tokenizer::read_pair_merges(const std::string& path){
+std::map<Tokenizer::StringPair, int> Tokenizer::read_pair_merges(const std::string& path){
     std::ifstream file(path, std::ios_base::in);
     if (!file.is_open()) {
         throw std::runtime_error("can't open the file");
     }
 
-    std::map<std::pair<std::string, std::string>, int> merges;
+    std::map<StringPair, int> merges;
     int rank = 0;
     for(std::string line; getline(file, line); ){
         unsigned int pos = line.find(' ');
-        std::pair<std::string, std::string> pair = std::make_pair(line.substr(0, pos), line.substr(pos));
+        StringPair pair = std::make_pair(line.substr(0, pos), line.substr(pos + 1));
         merges[pair] = rank;
         ++rank;
     }
@@ -49,6 +49,79 @@ std::map<std::pair<std::string, std::string>, int> Tokenizer::read_pair_merges(c
 }
 
 std::vector<std::string> Tokenizer::divide_to_subwords(const std::string &word) {
-    // TODO implement subdivision with merge ranks
-    return std::vector<std::string>();
+    std::vector<std::string> tokens(word.size());
+    for (int i = 0; i < word.size(); ++i){
+        tokens[i] = word[i];
+    }
+    std::vector<StringPair> pairs = get_character_pairs(tokens);
+    if (pairs.empty()){
+        return std::vector<std::string>{word};
+    }
+    while (true) {
+        StringPair bigram = *std::min_element(
+                pairs.begin(),
+                pairs.end(),
+                [this](const StringPair& a, const StringPair& b){return compare_by_rank(a, b);}
+                );
+        if (pair_merges_ranks.find(bigram) == pair_merges_ranks.end()) {
+            break;
+        }
+        std::string first = bigram.first;
+        std::string second = bigram.second;
+
+        std::vector<std::string> new_tokens{};
+        auto iter = tokens.begin();
+        while (iter != tokens.end()){
+            auto next_occurrence = std::find(iter, tokens.end(), first);
+            new_tokens.insert(new_tokens.end(), iter, next_occurrence);
+            iter = next_occurrence;
+            if (iter == tokens.end()){
+                break;
+            }
+
+            auto next = iter + 1;
+            if(*iter == first && next != tokens.end() && *next == second){
+                new_tokens.push_back(first + second);
+                iter = next + 1;
+            }else{
+                new_tokens.push_back(*iter);
+                ++iter;
+            }
+        }
+
+        tokens = new_tokens;
+        if (tokens.size() == 1){
+            break;
+        }
+        pairs = get_character_pairs(tokens);
+    }
+    return tokens;
+}
+
+//std::vector<Tokenizer::StringPair> Tokenizer::get_character_pairs(const std::string& word){
+//    std::vector<StringPair> pairs(word.size() - 1);
+//    for (int i = 0; i < word.size() - 1; ++i){
+//        pairs[i] = std::make_pair(word[i], word[i + 1]);
+//    }
+//    return pairs;
+//}
+
+std::vector<Tokenizer::StringPair> Tokenizer::get_character_pairs(const std::vector<std::string>& word){
+    std::vector<StringPair> pairs(word.size() - 1);
+    for (int i = 0; i < word.size() - 1; ++i){
+        pairs[i] = std::make_pair(word[i], word[i + 1]);
+    }
+    return pairs;
+}
+
+bool Tokenizer::compare_by_rank(const Tokenizer::StringPair &a, const Tokenizer::StringPair &b) {
+    auto fst = pair_merges_ranks.find(a);
+    auto snd = pair_merges_ranks.find(b);
+    if (fst == pair_merges_ranks.end()){
+        return false;
+    }
+    if (snd == pair_merges_ranks.end()){
+        return true;
+    }
+    return fst->second < snd->second;
 }
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -10,16 +10,21 @@
 
 class Tokenizer {
 
+    typedef std::pair<std::string, std::string> StringPair;
+
     std::map<std::string, int> vocabulary;
-    std::map<std::pair<std::string, std::string>, int> pair_merges;
+    std::map<StringPair, int> pair_merges_ranks;
 
 public:
     Tokenizer(const std::string& vocabulary_path, const std::string& pair_merges_path);
+    std::vector<std::string> divide_to_subwords(const std::string& word);
 
 private:
     static std::map<std::string, int> read_vocabulary(const std::string&);
-    static std::map<std::pair<std::string, std::string>, int> read_pair_merges(const std::string& path);
-    std::vector<std::string> divide_to_subwords(const std::string& word);
+    static std::map<StringPair, int> read_pair_merges(const std::string& path);
+//    static std::vector<StringPair> get_character_pairs(const std::string& word);
+    static std::vector<StringPair> get_character_pairs(const std::vector<std::string>& word);
+    bool compare_by_rank(const StringPair &a, const StringPair &b);
 };
 
 
